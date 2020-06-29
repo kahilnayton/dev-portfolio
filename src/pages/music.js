aaf8d0de347e2fc49cbefccbe817e8d89816a791
kahil nayton <kahilnayton@gmail.com>
2020-06-29 09:29:37 -0400

    removed contact success

--- src/pages/music.js
+++ src/pages/music.js
@@ -1,18 +1,16 @@
-import React from "react"
+import React from 'react';
 // import { Link, graphql } from "gatsby"
 // import { linkResolver } from "gatsby-source-prismic-graphql"
 // import { linkResolver } from '../utils/linkResolver'
 // import htmlSerializer from '../utils/htmlSerializer';
 
-import Layout from "../components/Layout"
-import RichText from "../components/RichText";
+import Layout from '../components/Layout';
+import RichText from '../components/RichText';
 
-
-export default (props) => {
-
+export default props => {
   return (
     <Layout>
-<h1>music</h1>
+      <h1>music</h1>
     </Layout>
-  )
-}
+  );
+};
