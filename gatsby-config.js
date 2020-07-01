279093d9cf91e29b75a7b80018d066e98a0c6c7a
kahil nayton <kahilnayton@gmail.com>
2020-06-30 23:35:52 -0400

    trying to get package to pass build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
 const siteMetadata = {
   title: `gatsby-prismic-blog`,
   description: `New portfolio blog`,
@@ -12,43 +16,21 @@ const siteMetadata = {
 
 module.exports = {
   siteMetadata,
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-image`,
     `gatsby-plugin-catch-links`,
+    `gatsby-plugin-sass`,
     `gatsby-plugin-resolve-src`,
     `gatsby-plugin-remove-trailing-slashes`,
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /vectors/,
-        }
-      }
-    },
+    `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `img`,
-        path: `${__dirname}/src/img`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/img/gatsby-icon.png`, // This path is relative to the root of the site.
+        path: `${__dirname}/src/images`,
       },
     },
     {
@@ -63,9 +45,29 @@ module.exports = {
             match: '/project/:uid',
             path: '/project',
             component: require.resolve('./src/templates/project.js'),
-          }
-        ]
+          },
+        ],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /vectors/,
+        }
       }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `kahil development`,
+        short_name: `kahil`,
+        start_url: `/`,
+        background_color: `#D32030`,
+        theme_color: `#D32030`,
+        display: `minimal-ui`,
+        icon: `src/images/face.JPG`, // This path is relative to the root of the site.
+      },
     },
   ],
 }
