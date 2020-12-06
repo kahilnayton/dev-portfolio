f6b37fd1772978ffe1850773fd4a08b2f691d218
kahilnayton <kahilnayton@gmail.com>
2020-12-06 13:15:51 -0500

    removed doc info and added mem game

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,12 +4,6 @@ import Layout from '../components/Layout';
 import styled from '@emotion/styled';
 import colors from 'styles/colors';
 import ContactForm from '../components/ContactForm';
-import { jsPDF } from 'jspdf';
-
-const doc = new jsPDF();
-
-doc.text = 'hello world';
-doc.save('a4.pdf');
 
 const ContactFormContainer = styled.div`
   padding: 5rem 0;
