bdaec91a1a9b547f790106b402f0529b0f3017d2
kahil nayton <kahilnayton@gmail.com>
2021-03-24 23:26:17 -0400

    added to layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 // import typeStyles from '../styles/typography';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import Head from 'next/head';
 
 const LayoutContainer = styled.div``;
 
@@ -14,6 +15,20 @@ const LayoutContent = styled.main`
 
 const Layout = ({ children }) => (
   <LayoutContainer>
+    <Head>
+      <meta
+        property="og:title"
+        content="Kahil Nayton Portfolio"
+      />
+      <meta
+        property="og:description"
+        content="Blog and Projects"
+      />
+      <meta
+        property="og:image"
+        content="https://miro.medium.com/fit/c/160/160/1*8s-r9WPNGv9o5s_ODQucqQ.jpeg"
+      />
+    </Head>
     {/* <Global styles={[resetStyles, globalStyles, typeStyles]} /> */}
 
     <div className="Layout">
